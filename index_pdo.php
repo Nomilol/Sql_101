0587fcd7f8b32b96d59e75026ec52ece8c17ae9d
Axel <lezinaxel@gmail.com>
2017-01-31 16:54:21 +0100

    Affichage des id des chaussettes roses

--- index_pdo.php
+++ index_pdo.php
@@ -5,7 +5,8 @@ ORM::configure('mysql:host=localhost;dbname=mon_armoire');
 ORM::configure('username', 'root');
 ORM::configure('password', 'root');
 
-$reponse = ORM::for_table('mes_chaussettes')->find_many();
+$reponse = ORM::for_table('mes_chaussettes')->where('couleur', 'rose')->find_many();
+//var_dump($reponse);
 foreach($reponse as $mes_chaussettes){
   echo $mes_chaussettes->id . "\n";
 }
